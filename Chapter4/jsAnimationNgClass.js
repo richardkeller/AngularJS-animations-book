d71a1dae7f9c8aa6976e14263371b800bed9f757
Richard Keller <richardhb@gmail.com>
2014-09-22 22:50:10 -0300

    Add chapter 4 samples to github pages

---
+++ Chapter4/jsAnimationNgClass.js
@@ -0,0 +1,64 @@
+﻿/**
+* @name firstJsAnimation
+* @desc The first sample animation function
+*/
+function firstJsAnimation() {
+
+    /**
+    * @name addClassAnimation
+    * @desc The animation function called when a class is removed from the element
+    * @param element - The element that will have the class removed
+    * @param className - The name of the class that will be removed from the element
+    * @param done - Callback function, it must be called to finish the animation
+    */
+    var addClassAnimation = function (element, className, done) {
+        //Check if the class added is the one that triggers the animation
+        if (className != 'animationClass') {
+            return;
+        }
+
+        //Animate to slide up and then call done function
+        jQuery(element).slideUp(300, done);
+
+        // Here is the optional return function that treats completed or cancelled animations
+        return function (isCancelled) {
+            if (isCancelled) {
+                element.stop();
+            }
+        };
+    }
+
+    /**
+    * @name removeClassAnimation
+    * @desc The animation function called when a class is removed from the element
+    * @param element - The element that will have the class removed
+    * @param className - The name of the class that will be removed from the element
+    * @param done - Callback function, it must be called to finish the animation
+    */
+    var removeClassAnimation = function (element, className, done) {
+        //Check if the class removed is the one that triggers the animation
+        if (className != 'animationClass') {
+            return;
+        }
+
+        //Animate to slide down and then call done function
+        jQuery(element).slideDown(300, done);
+
+
+        // Here is the optional return function that treats completed or cancelled animations
+        return function (isCancelled) {
+            if (isCancelled) {
+                element.stop();
+            }
+        };
+    }
+
+    return {
+        addClass: addClassAnimation,
+        removeClass: removeClassAnimation
+    };
+
+}
+
+var app = angular.module('myApp', ['ngAnimate'])
+    .animation(".firstJsAnimation", firstJsAnimation);
---
+++ Chapter4/jsAnimationNgRepeat.js
@@ -0,0 +1,51 @@
+﻿/**
+* @name repeatControllerFn
+* @desc Repeat sample controller
+*/
+function repeatControllerFn() {
+    var rc = this;
+    rc.items = [{ name: 'David' }, { name: 'Adailton' }, { name: 'Claudio' }, { name: 'Cleomar' }, { name: 'Filipe' }];
+
+    /**
+    * @name sortItems
+    * @desc Sort items array
+    */
+    rc.sortItems = function () {
+        rc.items.sort(function (a, b) { return a[name] < b[name] ? -1 : 1 });
+    };
+}
+
+/**
+* @name repeatItemAnimation
+* @desc The ngRepeat sample animation function
+*/
+function repeatItemAnimation() {
+    /**
+    * @name moveAnimation
+    * @desc The move animation function called when an element moves in DOM
+    * @param element - The element that is moving in DOM
+    * @param done - Callback function, it must be called to finish the animation
+    */
+    var moveAnimation = function (element, done) {
+        jQuery(element).css({ opacity: 0 });
+        jQuery(element).animate({
+            opacity: 1
+        }, 3000, done);
+
+        // Here is the optional return function that treats completed or cancelled animations
+        return function (isCancelled) {
+            if (isCancelled) {
+                element.stop();
+            }
+        };
+    }
+
+    return {
+        move: moveAnimation
+    };
+}
+
+
+var app = angular.module('myApp', ['ngAnimate'])
+    .controller("repeatController", repeatControllerFn)
+    .animation(".repeatItemAnimation", repeatItemAnimation);
